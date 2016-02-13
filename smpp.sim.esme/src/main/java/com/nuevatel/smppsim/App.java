678be32c50687db07f7958f2394f13ddc622574c
Ariel Salazar <ariel.salazar@nuevatel.com>
2016-02-13 14:33:24 -0400

    Initial commit

---
+++ smpp.sim.esme/src/main/java/com/nuevatel/smppsim/App.java
@@ -0,0 +1,13 @@
+package com.nuevatel.smppsim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
